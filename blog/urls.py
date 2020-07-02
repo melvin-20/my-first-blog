30086f913acf6f9579d68022b98e4b12aadbf7c8
Melvinne <rhodamelvynah20@gmail.com>
2020-07-02 07:44:10 +0300

    Modified the frontend

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path #We are importing djangos function path and all of our views from the blog application
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
-	path('', views.post_list, name = 'post_list'), # We are assigning a view called post_list to the root url
+	path('', views.post_list, name = 'post_list'),
+	 # We are assigning a view called post_list to the root url
 ]
 
 """As you can see, we're now assigning a view called post_list to the root URL. This URL pattern will match an empty string and the Django URL resolver will ignore the domain name (i.e., http://127.0.0.1:8000/) that prefixes the full URL path. This pattern will tell Django that views.post_list is the right place to go if someone enters your website at the 'http://127.0.0.1:8000/' address.
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Post # The dot means current directory or current application
 
 # Create your views here.
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
+	return render(request, 'blog/post_list.html', {'posts': posts})
 
 """A view is a place where we put the "logic" of our application. It will request information from the model you created before and pass it to a template. We created a function (def) called post_list that takes request and will return the value it gets from calling another function render that will render (put together) our template blog/post_list.html."""
